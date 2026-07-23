7b27e786c6b6b6758e892347da30891aaf84b717
Hchnr <hechenrui123@gmail.com>
2026-07-23 18:26:56 +0800

    Remove --wandb-entity CLI override from efficiency_cmp.sh, use config default

--- scripts/efficiency_cmp.sh
+++ scripts/efficiency_cmp.sh
@@ -24,7 +24,6 @@ CUDA_VISIBLE_DEVICES=0,1 torchrun --nproc_per_node=$NPROC --master_port=29500 -m
     --output-dir outputs/exp1_no_compile_no_cudagraph \
     --log-dir logs/exp1_no_compile_no_cudagraph \
     --wandb-project wm-training \
-    --wandb-entity ybainlp \
     --wandb-name new_repo_exp1-no-compile-no-cudagraph \
     > logs/exp1_no_compile_no_cudagraph.log 2>&1 &
 PID1=$!
@@ -40,7 +39,6 @@ CUDA_VISIBLE_DEVICES=2,3 torchrun --nproc_per_node=$NPROC --master_port=29501 -m
     --output-dir outputs/exp2_compile_no_cudagraph \
     --log-dir logs/exp2_compile_no_cudagraph \
     --wandb-project wm-training \
-    --wandb-entity ybainlp \
     --wandb-name new_repo_exp2-compile-no-cudagraph \
     > logs/exp2_compile_no_cudagraph.log 2>&1 &
 PID2=$!
@@ -55,8 +53,7 @@ CUDA_VISIBLE_DEVICES=4,5 torchrun --nproc_per_node=$NPROC --master_port=29502 -m
     --adam-fused false \
     --output-dir outputs/exp3_compile_cudagraph \
     --log-dir logs/exp3_compile_cudagraph \
-    --wandb-project wm-raw-ablation \
-    --wandb-entity ybainlp \
+    --wandb-project wm-training \
     --wandb-name new_repo_exp3-compile-cudagraph \
     > logs/exp3_compile_cudagraph.log 2>&1 &
 PID3=$!
@@ -71,8 +68,7 @@ CUDA_VISIBLE_DEVICES=6,7 torchrun --nproc_per_node=$NPROC --master_port=29503 -m
     --adam-fused true \
     --output-dir outputs/exp4_compile_cudagraph_fused \
     --log-dir logs/exp4_compile_cudagraph_fused \
-    --wandb-project wm-raw-ablation \
-    --wandb-entity ybainlp \
+    --wandb-project wm-training \
     --wandb-name new_repo_exp4-compile-cudagraph-fused \
     > logs/exp4_compile_cudagraph_fused.log 2>&1 &
 PID4=$!
